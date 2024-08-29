a4bb3957940d80d5a1e90a0fecf91d3bcd11de31
prithika0928 <pprithika264@gmail.com>
2024-08-29 17:07:23 +0530

    racipe

---
+++ dhuddu/src/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+       <h1 className='head'>Food</h1>
+    <p className='more'>Home Contact Registered</p>
+   
+        </div>
+  )
+}
---
+++ dhuddu/src/App.js
@@ -0,0 +1,13 @@
+
+import { About } from './About';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+  <About />
+    </div>
+  );
+}
+
+export default App;
